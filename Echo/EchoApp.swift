7d170aaf5e6b855e18cb9831768681f3bce19687
Om Preetham <ompreethambandi@gmail.com>
2024-08-03 15:30:33 -0500

    Initial Commit

---
+++ Echo/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo
+//
+//  Created by Om Preetham Bandi on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct EchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
